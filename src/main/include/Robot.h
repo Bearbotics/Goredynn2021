aa6ec057aa4a418a540f78a07a1928dc99c0c75a
wm-a <unconfigured@null.spigotmc.org>
2021-04-04 17:19:30 -0500

    Fixed Shuffleboard

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -16,74 +16,89 @@
 #include <networktables/NetworkTable.h>
 
 //create class of robot with parent
-class Robot : public frc::TimedRobot {
+class Robot : public frc::TimedRobot
+{
 
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
+public:
+    void RobotInit() override;
+    void RobotPeriodic() override;
 
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
+    void AutonomousInit() override;
+    void AutonomousPeriodic() override;
 
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
+    void TeleopInit() override;
+    void TeleopPeriodic() override;
 
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
+    void DisabledInit() override;
+    void DisabledPeriodic() override;
 
-  void TestInit() override;
-  void TestPeriodic() override;
+    void TestInit() override;
+    void TestPeriodic() override;
 
-  private:
+private:
     // Initialize pigeonIMU from device ID pigeon ID
-    ctre::phoenix::sensors::PigeonIMU pigeon_ {pigeonId};
+    ctre::phoenix::sensors::PigeonIMU pigeon_{pigeonId};
 
     // Motor Intializers
-    rev::CANSparkMax frMotor_          {frMotorId          ,rev::CANSparkMax::MotorType::kBrushless};
-    rev::CANSparkMax flMotor_          {flMotorId          ,rev::CANSparkMax::MotorType::kBrushless};
-    rev::CANSparkMax rrMotor_          {rrMotorId          ,rev::CANSparkMax::MotorType::kBrushless};
-    rev::CANSparkMax rlMotor_          {rlMotorId          ,rev::CANSparkMax::MotorType::kBrushless};
-    rev::CANSparkMax intakeMotor_      {intakeMotorId      ,rev::CANSparkMax::MotorType::kBrushed  };
-    rev::CANSparkMax escalatorMotor_   {escalatorMotorId   ,rev::CANSparkMax::MotorType::kBrushed  };
-    rev::CANSparkMax feederMotor_      {feederMotorId      ,rev::CANSparkMax::MotorType::kBrushed  };
-    rev::CANSparkMax shooterMotorOne_  {shooterMotorOneId  ,rev::CANSparkMax::MotorType::kBrushed  };
-    rev::CANSparkMax shooterMotorTwo_  {shooterMotorTwoId  ,rev::CANSparkMax::MotorType::kBrushed  };
-    frc::SpeedControllerGroup shooterMotors_ {shooterMotorOne_, shooterMotorTwo_};
+    rev::CANSparkMax frMotor_{frMotorId, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax flMotor_{flMotorId, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax rrMotor_{rrMotorId, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax rlMotor_{rlMotorId, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax intakeMotor_{intakeMotorId, rev::CANSparkMax::MotorType::kBrushed};
+    rev::CANSparkMax escalatorMotor_{escalatorMotorId, rev::CANSparkMax::MotorType::kBrushed};
+    rev::CANSparkMax feederMotor_{feederMotorId, rev::CANSparkMax::MotorType::kBrushed};
+    rev::CANSparkMax shooterMotorOne_{shooterMotorOneId, rev::CANSparkMax::MotorType::kBrushed};
+    rev::CANSparkMax shooterMotorTwo_{shooterMotorTwoId, rev::CANSparkMax::MotorType::kBrushed};
+    frc::SpeedControllerGroup shooterMotors_{shooterMotorOne_, shooterMotorTwo_};
 
     // Initialize Joysticks
-    frc::Joystick firstJoystick_    {firstJoystickId};
-    frc::Joystick secondJoystick_   {secondJoystickId};
+    frc::Joystick firstJoystick_{firstJoystickId};
+    frc::Joystick secondJoystick_{secondJoystickId};
 
     // Initialize MecanumDrive object called mecanumDrive_
-    frc::MecanumDrive mecanumDrive_ {frMotor_, flMotor_, rrMotor_, rlMotor_};
+    frc::MecanumDrive mecanumDrive_{frMotor_, flMotor_, rrMotor_, rlMotor_};
 
+    // ------------------------
     // Add mecanum display to shuffleboard
-    frc::ComplexWidget* mecanumShuffleboard = &frc::Shuffleboard::GetTab(tabName)
-        .Add("Mecanum Drive",mecanumDrive_);
+    frc::ComplexWidget *mecanumShuffleboard = &frc::Shuffleboard::GetTab(tabName)
+                                                   .Add("Mecanum Drive", mecanumDrive_)
+                                                   .WithWidget(frc::BuiltInWidgets::kMecanumDrive)
+                                                   .WithPosition(4,2)
+                                                   .WithSize(4,2);
 
     // Initialize a bunch of shuffleboard network tables
     // for the speeds that everything should be set at
     nt::NetworkTableEntry intakeSpeed_ = frc::Shuffleboard::GetTab(tabName)
-        // AddPersistent makes sure that if the controller is set,
-        // next time shuffleboard loads up, it'll use that as the
-        // default value
-        .AddPersistent("Intake Motor", 0.0)
-        // Builtin Widget made to set the speed of speed controllers
-        .WithWidget(frc::BuiltInWidgets::kSpeedController)
-        // Get Network Table that will be dynamically updated
-        .GetEntry();
+                                             // AddPersistent makes sure that if the controller is set,
+                                             // next time shuffleboard loads up, it'll use that as the
+                                             // default value
+                                             .AddPersistent("Intake Motor", 0.0)
+                                             // Builtin Widget made to set the speed of speed controllers
+                                             .WithWidget(frc::BuiltInWidgets::kNumberSlider)
+                                             // Set Position of widget
+                                             .WithPosition(1, 1)
+                                             // Adding a Size?
+                                             .WithSize(2,1)
+                                             // Get Network Table that will be dynamically updated
+                                             .GetEntry();
     nt::NetworkTableEntry escalatorSpeed_ = frc::Shuffleboard::GetTab(tabName)
-        .AddPersistent("Escalator Motor", 0.0)
-        .WithWidget(frc::BuiltInWidgets::kSpeedController)
-        .GetEntry();
+                                                .AddPersistent("Escalator Motor", 0.0)
+                                                .WithWidget(frc::BuiltInWidgets::kNumberSlider)
+                                                .WithPosition(1, 2)
+                                                .WithSize(2,1)
+                                                .GetEntry();
     nt::NetworkTableEntry feederSpeed_ = frc::Shuffleboard::GetTab(tabName)
-        .AddPersistent("Feeder Motor", 0.0)
-        .WithWidget(frc::BuiltInWidgets::kSpeedController)
-        .GetEntry();
+                                             .AddPersistent("Feeder Motor", 0.0)
+                                             .WithWidget(frc::BuiltInWidgets::kNumberSlider)
+                                             .WithPosition(1, 3)
+                                             .WithSize(2,1)
+                                             .GetEntry();
     nt::NetworkTableEntry shooterSpeed_ = frc::Shuffleboard::GetTab(tabName)
-        .AddPersistent("Shooter Motor", 0.0)
-        .WithWidget(frc::BuiltInWidgets::kSpeedController)
-        .GetEntry();
-    
+                                              .AddPersistent("Shooter Motor", 0.0)
+                                              .WithWidget(frc::BuiltInWidgets::kNumberSlider)
+                                              .WithPosition(1, 4)
+                                              .WithSize(2,1)
+                                              .GetEntry();
+    // ----------------
     // frc::PowerDistributionPanel pdp_{}
 };
